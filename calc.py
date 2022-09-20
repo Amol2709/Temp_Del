2deff2b84f4202da7f71daf6b3b60a26c418387d
Amol <includeamol@gmail.com>
2022-09-20 10:53:03 +0530

    Added two function

--- calc.py
+++ calc.py
@@ -1,11 +1,19 @@
 def add(x,y):
+    '''
+    '''
     return x+y
 
-
-
 def mul(x,y):
+    '''
+    '''
     return x*y
 
 def sub(x,y):
     return x-y
 
+
+
+def func1():
+    pass 
+def func2():
+    pass
